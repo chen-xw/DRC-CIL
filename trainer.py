14f20577f59acf9cd400b1880fd9065c0a925005
chen-xw <80995997+chen-xw@users.noreply.github.com>
2023-08-07 20:24:22 +0800

    Add files via upload

---
+++ trainer.py
@@ -0,0 +1,96 @@
+import sys
+import os
+import logging
+import copy
+import torch
+from utils import factory
+from utils.data_manager import DataManager
+from utils.toolkit import count_parameters
+
+
+def train(args):
+    seed_list = copy.deepcopy(args['seed'])
+    device = copy.deepcopy(args['device'])
+
+
+    for seed in seed_list:
+        args['seed'] = seed
+        args['device'] = device
+        _train(args)
+
+
+def _train(args):
+
+    logs_name = "logs/{}/".format(args["model_name"])
+    if not os.path.exists(logs_name):
+        os.makedirs(logs_name)
+ 
+    logfilename = 'logs/{}/{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}'.format(args["model_name"], args['prefix'],  args['seed'], args['model_name'], args["convnet_type"],
+                                                args['dataset'], args['init_cls'], args['increment'], args["device"],
+                                                args['alpha'], args['beta1'], args['beta2'], args["tro"], args['save_path'],args["pretrain"])
+        
+    
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s [%(filename)s] => %(message)s',
+        handlers=[
+            logging.FileHandler(filename=logfilename + '.log'),
+            logging.StreamHandler(sys.stdout)
+        ]
+    )
+
+    _set_random()
+    _set_device(args)
+    print_args(args)
+    data_manager = DataManager(args['dataset'], args['shuffle'], args['seed'], args['init_cls'], args['increment'],args['model_name'])
+    model = factory.get_model(args['model_name'], args)
+
+    cnn_curve, nme_curve = {'top1': [], 'top5': []}, {'top1': [], 'top5': []}
+    for task in range(data_manager.nb_tasks):
+        logging.info('All params: {}'.format(count_parameters(model._network)))
+        logging.info('Trainable params: {}'.format(count_parameters(model._network, True)))
+        if args["pretrain"] :
+            model.incremental_test(data_manager)
+        else:
+            model.incremental_train(data_manager)
+        cnn_accy = model.eval_task()
+        model.after_task()
+
+        if cnn_accy is not None:
+            cnn_curve['top1'].append(cnn_accy['top1'])
+            cnn_curve['top5'].append(cnn_accy['top5'])
+
+            logging.info('CNN top1 curve: {}'.format(cnn_curve['top1']))
+            logging.info('CNN top5 curve: {}'.format(cnn_curve['top5']))
+
+    logging.info('CNN top1 average accuracy: {}'.format(sum(cnn_curve['top1'])/len(cnn_curve['top1'])))
+    logging.info('CNN top5 average accuracy: {}'.format(sum(cnn_curve['top5'])/len(cnn_curve['top5'])))
+
+
+def _set_device(args):
+    device_type = args['device']
+    gpus = []
+
+    for device in device_type:
+        if device_type == -1:
+            device = torch.device('cpu')
+        else:
+            device = torch.device('cuda:{}'.format(device))
+
+        gpus.append(device)
+
+    args['device'] = gpus
+
+
+def _set_random():
+    torch.manual_seed(1)
+    torch.cuda.manual_seed(1)
+    torch.cuda.manual_seed_all(1)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def print_args(args):
+    for key, value in args.items():
+        logging.info('{}: {}'.format(key, value))
